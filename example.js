1f2490e14fa2e7ba9a42c0e5205c1c8a3695cd53
Surendra Singhi <ssinghi@kreeti.com>
2010-09-04 00:38:49 +0530

    extend code

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ users = new RDX.Table(cp, 'users');
 collections = new RDX.Table(cp, 'collections');
 
 users.has_many('collections', collections, 'user_id')
-x = users.query().where('id = 675011 ').execute()[0];
+x = users.query().where('id = 675011 ').executeSync()[0];
 console.log(x)
 x.first_name = "S";
 x.save()
