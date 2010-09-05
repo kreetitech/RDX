ba5579568fb7847ffbcab724faf5c1d27c09ce23
Surendra Singhi <ssinghi@kreeti.com>
2010-09-05 13:18:46 +0530

    add belongs to relationship

--- docs/example.js
+++ docs/example.js
@@ -1,5 +1,6 @@
 require.paths.push(__dirname);
 require.paths.push(__dirname + '/..');
+require.paths.push(__dirname + '/../deps');
 
 var RDX = require('index').RDX;
 var cfg = require("tests/fixtures/config").cfg;
@@ -8,6 +9,7 @@ var ConnectionPool = RDX.ConnectionPool;
 var Table = RDX.Table;
 var Query = RDX.Query;
 var ToMany = RDX.ToMany;
+var BelongsTo = RDX.BelongsTo;
 
 var cp = new ConnectionPool(cfg);
 var users = new Table(cp, 'users');
@@ -18,6 +20,8 @@ users.collections = users.prototype.collections = new ToMany(users, 'id', collec
 
 collections.items  = new ToMany(collections, 'id', items, 'collection_id');
 
+collections.prototype.user = collections.user = new BelongsTo(collections, 'user_id', users, 'id');
+
 q = users.query().where('id = 675011 ');
 
 x = q.executeSync()[0];
@@ -26,6 +30,14 @@ q = x.collections().query();
 console.log(q.toSql());
 
 q = users.collections().query();
+console.log(q.toSql());
+
+c = q.take(1).executeSync()[0];
+console.log(c instanceof collections);
+
+console.log(c.user().query().toSql());
+
+q = collections.user().query();
 console.log(q.toSql());
 
 q = collections.items().query();
--- index.js
+++ index.js
@@ -3,5 +3,6 @@ RDX.ConnectionPool = require('./lib/connection').ConnectionPool;
 RDX.Table = require('./lib/table').Table;
 RDX.Query = require('./lib/table').Query;
 RDX.ToMany = require('./lib/to_many').ToMany;
+RDX.BelongsTo = require('./lib/belongs_to').BelongsTo;
 
 
