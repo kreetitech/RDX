5b37fba16eb2aeb9e88ad9dce5ac3f347533ddf8
Surendra Singhi <ssinghi@kreeti.com>
2010-11-19 01:41:28 +0530

    update test models slightly

--- tests/collection.js
+++ tests/collection.js
@@ -1,4 +1,9 @@
 var cp = require('./setup').RDXTestConnectionPool();
 var Table = require('table').Table;
+var Item = require('./item').Item;
+var User = require('./user').User;
 
 var Collection = exports.Collection = new Table(cp, 'collections');
+
+Collection.toMany('items', Item, Item.fields["collection_id"]);
+console.log(Collection.items.query().toSql())
--- tests/user.js
+++ tests/user.js
@@ -1,3 +1,6 @@
 var cp = require('./setup').RDXTestConnectionPool;
 var Table = require('table').Table;
+var Collection = require('./collection').Collection;
+
 var User = exports.User = new Table(cp(), 'users');
+User.toMany('collections', Collection, Collection.fields["user_id"]);
