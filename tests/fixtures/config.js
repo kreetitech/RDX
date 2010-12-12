e39b668e763b41985af8c084328de703d3806a44
Surendra Singhi <ssinghi@kreeti.com>
2010-12-12 14:06:46 +0530

    add adapter config

--- tests/fixtures/config.js
+++ tests/fixtures/config.js
@@ -1,5 +1,5 @@
 exports.cfg = {
-  mysql: require('node-mysql-libmysqlclient/mysql-libmysqlclient'),
+  adapter: 'mysql-libmysqlclient',
   host: "localhost",
   user: "root",
   password: "Mapinkbo",
